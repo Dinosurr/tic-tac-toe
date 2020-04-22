8447861be47e8f8343efa7bd9777258802bd3ae4
Simon Ejdre <9076128+Dinosurr@users.noreply.github.com>
2020-04-22 20:39:55 +0200

    Update score.py

--- score.py
+++ score.py
@@ -1,14 +1,14 @@
-cross_wins = 3
+cross_wins = 0
 circle_wins = 0
 draws = 0
 
 average_circle_moves = 0
-average_cross_moves = 7.0
-average_moves = 7.0
+average_cross_moves = 0
+average_moves = 0
 
 
 
 
-all_moves = [5, 9, 7]
+all_moves = []
 circle_moves = []
-cross_moves = [5, 9, 7]
+cross_moves = []
